8e54ff064d0b2b9e3b39f805e00cfa6af4972838
Colton Lacey and Dustin Herboldshimer <colton@>
2019-05-01 14:34:25 -0700

    init commit

---
+++ Tamagotchi/Controllers/TamagotchisContollers.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using TravelDiary.Models;
+using System.Collections.Generic;
+
+
+namespace Tamagotchi.Controllers
+{
+  public class TamagotchisController : Controller
+  {
+    [HttpGet("/tamagotchis")]
+    public ActionResult Index()
+    {
+      List<Tamagotchi> allTamagotchis = Tamagotchi.GetAll();
+      return View(allTamagotchis);
+    }
+    [HttpGet("/tamagotchis/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/tamagotchis")]
+    public ActionResult Create(string name)
+    {
+      Tamagotchi myTamagotchi = new Tamagotchi(name;
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/tamagotchis/{id}")]
+    public ActionResult Show(int id)
+    {
+      Tamagotchi tamagotchis = Tamagotchi.Find(id);
+      return View(tamagotchis);
+    }
+
+  }
+}
---
+++ Tamagotchi/Models/Tamagotchis.cs
@@ -0,0 +1,53 @@
+using System.Collection.Generic;
+using System;
+
+namespace Tamagotchi.Models
+{
+  public class Tamagotchi
+  {
+    private string _name;
+    private int _health;
+    private int _food;
+    private int _attention;
+    private int _rest;
+    private static List<Tamagotchi> _instances = new List<Tamagotchi> {};
+
+    public string Name {get=> _name; set=> _name = value;}
+    public int Health {get=> _health; set=> _health = value;}
+    public int Food {get=> _food; set=> _food = value;}
+    public int Attention {get=> _attention; set=> _attention = value;}
+    public int Rest {get=> _rest; set=> _rest = value;}
+
+    public Tamagotchi (string name)
+    {
+      _name = name;
+      _health = 100;
+      _food = 100;
+      _attention = 100;
+      _rest = 100;
+      _instances.Add(this);
+      _id = _instances.count;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static Tamagotchi Find(int searchId)
+    {
+      for(int i = 0; i < _instances.Count; i++)
+      {
+        if(_instances[i].GetId() == searchId)
+        {
+          return _instances[i];
+        }
+      }
+      return _instances[0];
+    }
+    public static List<Place> GetAll()
+    {
+      return _instances;
+    }
+  }
+}
